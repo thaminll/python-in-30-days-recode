dfb09d3df8f3293c3e774452db861b51373a10da
samin lashgari <saminlashgari0@gmail.com>
2022-08-12 14:30:45 +0430

    data types

---
+++ day1.py
@@ -0,0 +1,28 @@
+#2
+print(3 + 4)
+print(4 - 3)
+print(4 * 3)
+print(4 % 3)
+print(4 / 3)
+print(4 ** 3)
+print(4 // 3)
+
+#3
+first_name = 'Samin'
+last_name = 'Lashgari'
+country = 'Iran'
+sentence = 'I am enjoying 30 days of python'
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['asabaneh', 'python', 'finland']))
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+
+#3-2
+dist = ((2 - 10)**2 + (3 - 8)**2) ** 1/2
+print(dist)
