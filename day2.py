550dd0e40e38bae24a0bc16e05e686792b0df71d
samin lashgari <saminlashgari0@gmail.com>
2022-09-02 15:15:43 +0430

    variables and built_in_function

---
+++ day2.py
@@ -0,0 +1,49 @@
+#day2 - level 1
+# 2 
+# 'day 2: 30 days of python programming'
+#3-13
+first_name = 'samin'
+last_name = 'lashgari'
+full_name = first_name + last_name
+country = 'Iran'
+city = 'tehran'
+age = 22
+year = 1999
+is_married = False
+is_true = 'in love'
+is_light_on = True
+name, lastname, age = "ali", "mosavi", 26
+
+#level 2
+#1-7
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(country))
+
+print(len(first_name))
+len_las= len(last_name)
+print(first_name > last_name)
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area = 2 * 3.14 * radius
+circumference = 3.14 * radius * 2
+radiuss = int(input("please enter the radius: "))
+area_cir = 2 * 3.14 * radiuss
+print(f"area = {area_cir} " )
+
+f_name = input("enter the name: ")
+l_name = input("enter last name: ")
+
+help("keywords")
+
+ 
