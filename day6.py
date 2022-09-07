cb9d5811dc00a154cb4aaea3a545772a3c917b98
samin lashgari <saminlashgari0@gmail.com>
2022-09-07 16:05:29 +0430

    tuple

---
+++ day6.py
@@ -0,0 +1,16 @@
+tpl = tuple()
+sisters = ('samin', 'sana')
+brother = ('Amirhosein', 'amo')
+sibling = sisters + brother
+print(len(sibling))
+
+fruits = ('banana' , 'pineapple', 'apple')
+vegetables = ('potato', 'tomato')
+food = fruits + vegetables
+print(food)
+
+food_ls = list(food)
+print(food[:2])
+del food
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
