c208d01f0b74e9c25454208442dce65855055937
samin lashgari <saminlashgari0@gmail.com>
2022-10-16 16:03:38 +0330

    dictionaries

---
+++ day8.py
@@ -0,0 +1,30 @@
+person = {'first_name' : 'Thamin', 'last_name' : 'Lashgari', 'age' : 23, 'major' : 'computer enginering', 'married' : False,
+'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+        }}
+print(len(person))
+print(person["major"])
+#print(person["ages"])
+print(person.get('age'))
+person['job_experience'] = 2
+person['skills'].append('linux')
+print(person)
+
+print('skills' in person)
+
+print(person.pop('last_name'))
+person.popitem()
+del person["age"]
+print(person)
+
+print(person.items())
+print(person.keys())
+print(person.values())
+
+
+print(type(person['skills']))
+person['skills'].append('network')
+print(person.values())
+print(person.items())
